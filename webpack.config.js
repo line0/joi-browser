e543cc260251fb965f8c4cac18d3e853cc6fd966
line0 <line0@mail.lv>
2017-09-07 14:27:04 +0200

    update webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,16 +16,32 @@ module.exports = {
       {
         // need to babelify joi, isemail, hoek, and topo's lib
         test: /[\\\/]node_modules[\\\/](joi[\\\/]lib[\\\/]|isemail[\\\/]lib[\\\/]|hoek[\\\/]lib[\\\/]|topo[\\\/]lib[\\\/])/,
-        loader: 'babel'
+        loader: 'babel-loader',
+        query: {
+          "plugins": [
+            "transform-es2015-modules-commonjs",
+            "transform-object-assign",
+            "transform-proto-to-assign",
+            [
+              "transform-es2015-classes",
+              {
+                "loose": true
+              }
+            ]
+          ],
+          "presets": [
+            "es2015"
+          ]
+        }
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel-loader'
       },
       {
         test: /\.json$/,
-        loader: 'json'
+        loader: 'json-loader'
       }
     ]
   },
